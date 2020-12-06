3cd78b8a8ce83f00c7a2733619a8c685c69851eb
harry <abc@gmail.com>
2020-12-06 15:04:41 -0500

    update path

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 mkdir /usr/local/sss
 cd /usr/local/sss
+
+curl https://raw.githubusercontent.com/harryyuanfeng/ubuntu_screenshot_on_click/main/index.js --output /usr/local/sss/index.js
+
 apt-get install scrot -y
 npm install pm2@latest -g
 npm init -y
@@ -8,6 +11,4 @@ npm install node-mouse --save
 mkdir -p $HOME/Pictures/Screenshots/
 chmod 777 /dev/input/mice
 
-curl https://raw.githubusercontent.com/harryyuanfeng/ubuntu_screenshot_on_click/main/index.js --output index.js
-
-pm2 start index.js
+pm2 start usr/local/sss/index.js
